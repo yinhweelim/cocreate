0444976945cac7f586f3409e69676f6af7d4ce95
hweebie <limyinhwee@gmail.com>
2023-09-08 16:14:28 +0800

    Add get users by auth_id API

--- back-end/src/routers/users.ts
+++ back-end/src/routers/users.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { updateUser, updateUserAvatar } from "../controllers/users";
+import {
+  updateUser,
+  updateUserAvatar,
+  getUsersByAuthId,
+} from "../controllers/users";
 import { auth } from "../middleware/auth";
 import { validation as checkValid } from "../middleware/checkValid";
 import { validateIdInParam, validateUpdateUserData } from "../validators/users";
 
 const router = express.Router();
 
+router.get("/users/:auth_id", getUsersByAuthId);
 router.patch(
   "/users/:id",
   validateIdInParam,
